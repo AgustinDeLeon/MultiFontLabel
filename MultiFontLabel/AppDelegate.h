2b1091cfddcd767fc7930afc3cc8efd16260c6c9
AgustinDeLeon <adeleon@codigodelsur.com>
2015-08-01 16:47:04 -0300

    Initial Commit

---
+++ MultiFontLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiFontLabel
+//
+//  Created by Agustin De Leon on 1/8/15.
+//  Copyright (c) 2015 Agustin De Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiFontLabel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultiFontLabel
+//
+//  Created by Agustin De Leon on 1/8/15.
+//  Copyright (c) 2015 Agustin De Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
