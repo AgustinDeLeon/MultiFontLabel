1d401fb0a40a358e2f39867ca4e9269178d789d2
AgustinDeLeon <adeleon@codigodelsur.com>
2015-08-02 21:08:33 -0300

    Creating project and first version of MultiFontLabel

---
+++ MultiFontLabel/LI_MultipleFontLabel.m
@@ -0,0 +1,21 @@
+//
+//  LI_MultipleFontLabel.m
+//  MultiFontLabel
+//
+//  Created by Agustin De Leon on 1/8/15.
+//  Copyright (c) 2015 Agustin De Leon. All rights reserved.
+//
+
+#import "LI_MultipleFontLabel.h"
+
+@implementation LI_MultipleFontLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
